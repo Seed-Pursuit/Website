231f7403da4228b340f275417fb7b2cd76978867
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-19 21:56:50 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
       <Router>
       <Routes>
+      <div>
+        <NavBar/>
+      </div>
         <Route path='/' element={<Home/>} exact/>
       </Routes>
       <div>
