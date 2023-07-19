bd843ce10dace5bf7e1c0576bca812a931892d67
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-19 18:30:30 +0530

    common components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} exact/>
+      </Routes>
+      <div>
+        <Footer/>
+      </div>
+      </Router>
     </div>
   );
 }
